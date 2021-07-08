e3e891a3b890dc38f9d137427de7e78bccb8d01c
samruddhiz <64641649+samruddhiz@users.noreply.github.com>
2021-07-08 16:51:00 +0530

    Update User.java

--- BankApplication/src/Org/User.java
+++ BankApplication/src/Org/User.java
@@ -9,10 +9,10 @@ public class User {
 		Nationalised_Bank branch1 = new Nationalised_Bank("MyNBank","Chennai");
 		//branch1.setBankName("MyBank");
 		branch1.openAccount();
-	   System.out.println("amount insured for Nationalised bank:" + 	branch1.amountInsured());
+	   System.out.println("amount insured for Nationalised bank:" + 	branch1.payInsuranceAmount());
 		
 		Cooperative_Bank branch2 = new Cooperative_Bank("MyCBank", "Mumbai");
 		branch2.openAccount();
-		System.out.println("amount insured for Nationalised bank:" + 	branch2.amountInsured());
+		System.out.println("amount insured for Nationalised bank:" + 	branch2.payInsuranceAmount());
 	}
 }
